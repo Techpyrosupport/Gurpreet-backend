3721e8d26f146a5d6e745fb0728274bf5950f8ea
vksr8102 <116914527+vksr8102@users.noreply.github.com>
2025-01-14 14:50:21 +0530

    public added in  gitignore

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(encryptResponseMiddleware)
+app.use(encryptResponseMiddleware)
 app.use(passport.initialize())
 app.use(session({
   secret: 'my-blog-secret',
